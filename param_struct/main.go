955acce7946a336b639d5ac43e1506a76d5f750a
jimmy <tv2169145@gmail.com>
2020-07-21 11:47:00 +0800

    try to use custom time type

--- param_struct/main.go
+++ param_struct/main.go
@@ -5,11 +5,63 @@ import (
 	"time"
 )
 
+//type MyTime time.Time
+//
+//func (mt *MyTime) UnmarshalJSON(bs []byte) error {
+//	var s string
+//	err := json.Unmarshal(bs, &s)
+//	if err != nil {
+//		return err
+//	}
+//	t, err := time.ParseInLocation("2006-01-02", s, time.UTC)
+//	if err != nil {
+//		return err
+//	}
+//	*mt = MyTime(t)
+//	return nil
+//}
+//
+//func (mt MyTime) MarshalJSON() ([]byte, error) {
+//	var stamp = fmt.Sprintf("\"%s\"", time.Time(mt).Format("2006-01-02"))
+//	return []byte(stamp), nil
+//}
+
+//--------------------------------------------------------------------------
+
+//type MyTime struct {
+//	time.Time
+//}
+//
+//func (ct *MyTime) UnmarshalJSON(b []byte) error {
+//	var err error
+//	s := strings.Trim(string(b), "\"")
+//	if s == "null" {
+//		ct.Time = time.Time{}
+//		return err
+//	}
+//	ct.Time, err = time.Parse("2006-01-02", s)
+//	return err
+//}
+//
+//func (ct *MyTime) MarshalJSON() ([]byte, error) {
+//	if ct.Time.UnixNano() == nilTime {
+//		return []byte("null"), nil
+//	}
+//	return []byte(fmt.Sprintf("\"%s\"", ct.Time.Format("2006-01-02"))), nil
+//}
+//
+//var nilTime = (time.Time{}).UnixNano()
+//func (ct *MyTime) IsSet() bool {
+//	return ct.UnixNano() != nilTime
+//}
+
+
 type Person struct {
 	Name string `json:"name" form:"name"`
 	Age int `json:"age" form:"age"`
-	Birthday time.Time `json:"birthday" form:"birthday" time_format:"2006-01-02" `
+	Birthday time.Time `json:"birthday" form:"birthday" time_format:"2006-01-02" time_utc:"1" `
 }
+
 
 func main() {
 	r := gin.Default()
